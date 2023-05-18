a0bd5d044af8ad1a606a0443ee448292f15fd8a4
Seif Eldin <seifeldinrefaat@gmail.com>
2023-05-18 13:13:05 +0300

    create user model

---
+++ src/model/User.ts
@@ -0,0 +1,15 @@
+import { Gender } from '../utils/enum/Gender';
+import { Role } from '../utils/enum/Role';
+
+interface User {
+  id?: string;
+  username: string;
+  email: string;
+  passwordHash: string;
+  gender?: Gender;
+  registeredAt?: Date;
+  lastLogin?: Date;
+  updatedAt?: Date;
+  isAdmin?: Role;
+  isActive: boolean;
+}
